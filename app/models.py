95c4cb3185877eedb052f08e8da7bd375bcaadc6
Qandaharov Ulugbek <ulugbekqand@gmail.com>
2022-10-10 16:29:34 +0500

    Fast Apini kerakli papkalrni ochish

---
+++ app/models.py
@@ -0,0 +1,6 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+from .database import Base
